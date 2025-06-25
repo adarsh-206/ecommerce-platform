59b22203323a9c02bbd984ab86d75c1dba6f9553
Adarsh Dhote <adarsh@Adarshs-MacBook-Air.local>
2025-06-25 23:22:21 +0530

    sitemap updated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,27 @@
-// next-sitemap.config.js (ES6 version)
+const getSlugs = async () => {
+  const res = await fetch("https://api.chaka-chak.in/blogs/get-slugs");
+  if (!res.ok) {
+    console.error("Failed to fetch slugs. Status:", res.status);
+    return [];
+  }
+  const data = await res.json();
+  console.log("Fetched blog slugs for sitemap:", data);
+  return data;
+};
+
 export default {
   siteUrl: "https://chaka-chak.in",
   generateRobotsTxt: true,
   sitemapSize: 5000,
   changefreq: "daily",
   priority: 0.7,
+  additionalPaths: async () => {
+    const slugs = await getSlugs();
+    return slugs.map((item) => ({
+      loc: `/blogs/${item.slug}`,
+      changefreq: "daily",
+      priority: 0.8,
+      lastmod: new Date().toISOString(),
+    }));
+  },
 };
